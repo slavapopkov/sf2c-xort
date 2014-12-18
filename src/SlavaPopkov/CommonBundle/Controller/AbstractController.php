a44f17defec062f584b6b1450128a8ebc9304446
slavapopkov <xort-s@mail.ru>
2014-12-18 22:30:36 +0400

    commit3

---
+++ src/SlavaPopkov/CommonBundle/Controller/AbstractController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SlavaPopkov\CommonBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+abstract  class AbstractController extends Controller
+{
+    
+}
---
+++ src/SlavaPopkov/WebsiteBundle/Controller/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SlavaPopkov\WebsiteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('WebsiteBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
