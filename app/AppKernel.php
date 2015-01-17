8a422151c319f7797b798da299ec457ec14495ab
slavapopkov <xort-s@mail.ru>
2015-01-17 22:54:47 +0400

    zadanie-1

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Magaz\CommonBundle\CommonBundle(),
+            new Magaz\WebsiteBundle\WebsiteBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
